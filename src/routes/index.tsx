6d5097637bcc1ef80734aa3cba5fe354d1a738ea
Egwuchukwu Stephen Diala <dialaegwuchukwu@gmail.com>
2024-11-26 23:17:43 +0100

    feat: requests page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,6 +4,7 @@ import { AnimatePresence } from "motion/react"
 import DashboardLayout from "@/layouts/ProtectedLayout"
 import { Routes, Route, BrowserRouter } from "react-router"
 import { DashboardPage } from "@/pages";
+import { RequestsPage } from "@/pages/requests";
 
 function LocationProvider({ children }: { children: ReactNode }) {
     return <AnimatePresence mode="wait">{children}</AnimatePresence>;
@@ -13,7 +14,8 @@ const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/*" element={<DashboardLayout><LocationProvider><DashboardPage /></LocationProvider></DashboardLayout>} />
+                <Route path="/" element={<DashboardLayout><LocationProvider><DashboardPage /></LocationProvider></DashboardLayout>} />
+                <Route path="/requests" element={<DashboardLayout><LocationProvider><RequestsPage /></LocationProvider></DashboardLayout>} />
             </Routes>
         </BrowserRouter>
     );
