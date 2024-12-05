b7480e6a6ddafb7b3b7ceaffa26885025401f883
Adejoke <adejokeogunyinka@enyata.com>
2024-12-05 17:56:56 +0100

    fix: build issue

--- src/constants/queryKeys.ts
+++ src/constants/queryKeys.ts
@@ -11,12 +11,11 @@ export const GET_DEPARTMENT_TIME_OFFS = "get-department-time-offs";
 export const GET_DEPARTMENT_TIME_OFF = "get-department-time-off";
 export const GET_DEPARTMENT_MATERIALS = "get-department-materials";
 export const GET_DEPARTMENT_MATERIAL = "get-department-material";
-export const GET_USERS = "get-users";
 
 // Users
 export const GET_USERS = "get-users";
 export const GET_USER = "get-user";
 
 // Connect Groups
-export const GET_CONNECT_GROUP = "get-connect-group"
-export const GET_CONNECT_GROUPS = "get-connect-groups"
+export const GET_CONNECT_GROUP = "get-connect-group";
+export const GET_CONNECT_GROUPS = "get-connect-groups";
